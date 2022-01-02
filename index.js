c47e899f97d4915b3be40aee0d4cd32d518cd33e
Edward Marion <edmarion6@gmail.com>
2022-01-02 03:42:01 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const main = document.querySelector("main");
+main.remove();
+
+const newHeader = document.createElement('h1');
+    newHeader.setAttribute("id", "victory")
+
+    newHeader.innerHTML = "Ed is the champion";
+
+newHeader.victory;
+
+document.body.append(newHeader);
